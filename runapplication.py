ba36e04235c59bd69e9d70be728b0a5a4a523921
Sandeep Raheja <raheja.sandeep@gmail.com>
2019-12-08 02:20:35 +0530

    run file updated

--- runapplication.py
+++ runapplication.py
@@ -7,4 +7,5 @@ from application.models import User, Post
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
 
-#app.run(debug=True, threaded=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True, host='0.0.0.0', port=5000)
